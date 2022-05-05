c8d87fe1900f17c311a64d1f273b0a045efe3614
Zotov Sergey <Zotov-Java@yandex.ru>
2022-05-05 17:12:41 +0300

    strings

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,19 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //Задача 1
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("Employee's full name - " + fullName);
+        //Задача 2
+        String s1 = fullName.toUpperCase();
+        System.out.println("Employee's full name data for filling out the report - " + s1);
+        //Задача 3
+        String fullName1 = "Ivanov Semyon Semyonovich";
+        fullName1 = fullName1.replace('y', 'i');
+        System.out.println("Employee's full name data - " + fullName1);
+
     }
 }
